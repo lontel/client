ea894a52da2659eed5a97470a96e9915dcefc774
Javier Ruiz <javierdeveloper1@gmail.com>
2022-07-27 11:29:04 +0200

    like button only for not owners

--- src/components/Chat/LikeButton.jsx
+++ src/components/Chat/LikeButton.jsx
@@ -1,12 +1,17 @@
-import { useState } from "react"
+import { useState, useContext } from "react"
+import { AuthContext } from "../../contexts/auth.context"
 
 
 const LikeButton = () => {
 
+    const { user } = useContext(AuthContext)
+
+    console.log(user, '----')
+
     const [counterValue, setCounterValue] = useState(0)
     const incrementValue = () => setCounterValue(counterValue + 1)
 
-
+    console.log(counterValue, 'EOEOEEOE')
     return (
         <>
             <button onClick={incrementValue} > Likes  {counterValue}</button>
