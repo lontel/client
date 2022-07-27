25b8817b923de804ff76a0bbba60b1305e3f99ea
Javier Ruiz <javierdeveloper1@gmail.com>
2022-07-27 13:22:05 +0200

    resolved problems with render likes

--- src/components/Chat/CardChat.jsx
+++ src/components/Chat/CardChat.jsx
@@ -37,7 +37,7 @@ const CardChat = ({ allMessages, loadMessages }) => {
                     <li>
                         <p>@{elm.owner.username}</p>
                         <img src={elm.owner.profilePic} alt="" />
-                        <p>{elm.message}</p>
+                        <p>{elm.message} {elm.likes} likes</p>
 
                         {
                             (elm.owner._id === user._id || user.role === 'ADMIN') ?
@@ -47,7 +47,7 @@ const CardChat = ({ allMessages, loadMessages }) => {
                                     <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
                                 </svg></Button>
                                 :
-                                <button onClick={() => incrementValue(elm._id)}> Likes {elm.likes}</button>
+                                <button onClick={() => incrementValue(elm._id)}> Touch for like </button>
 
                         }
                     </li>
